4b5e5c529abc8db1a8f52c115e724f1aed8fa6bd
Gary Scriver <gary.scriver@savvas.com>
2021-04-07 11:26:35 -0600

    Exposed some of the inner workings of ModelIterator to facilitate supper classes

--- src/main/java/com/github/bananaj/utils/ModelIterator.java
+++ src/main/java/com/github/bananaj/utils/ModelIterator.java
@@ -18,20 +18,22 @@ import com.github.bananaj.model.JSONParser;
 
 public class ModelIterator<T extends JSONParser> implements Iterable<T> {
 	
-	MailChimpConnection connection;
-	Queue<T> q = new LinkedList<T>();
+	protected MailChimpConnection connection;
+	protected Queue<T> q = new LinkedList<T>();
 	private String query;
 	private int offset = 0;
 	private int pagesize = 1000;
 	private Class<T> typeClasse;
-	private Integer totalItems;
+	protected Integer totalItems;
 	private int currentIndex = 0;
 	
 	public ModelIterator(Class<T> typeClasse, String query, MailChimpConnection connection) {
 		this.typeClasse = typeClasse;
 		this.connection = connection;
 		this.query = query;
-		readPagedEntities();
+		if (query != null) {
+			readPagedEntities();
+		}
 	}
 
 	public ModelIterator(Class<T> typeClasse, String query, MailChimpConnection connection, int pageSize) {
@@ -47,32 +49,8 @@ public class ModelIterator<T extends JSONParser> implements Iterable<T> {
 			URL url = new URL(query + (query.contains("?") ? "&" : "?") + "count="+pagesize + "&offset="+offset);
 			offset += pagesize;
 			final JSONObject list = new JSONObject(connection.do_Get(url,connection.getApikey()));
-
-			if (list.has("total_items")) {
-				totalItems = new Integer(list.getInt("total_items"));	// The total number of items matching the query regardless of pagination
-			}
-
-			Iterator<String> keys = list.keys();
-			while(keys.hasNext()) {
-				final String key = keys.next();
-				if (key.equals("_links")) { continue; }
-				final Object keyValue = list.get(key);
-				if (keyValue instanceof JSONArray) { // look for main entity array
-					// TODO: TRACE -- found 'key' entity array of type T
-					final JSONArray entArray = (JSONArray)keyValue;
-					for (int i = 0 ; i < entArray.length();i++)
-					{
-						final JSONObject objDetail = entArray.getJSONObject(i);
-						T ent = typeClasse.newInstance();
-						ent.parse(connection, objDetail);
-						q.offer(ent);
-					}
-					break;	// found entity array, no need to keep looking
-				}
-			}
-		} catch (InstantiationException e) {
-			throw new RuntimeException("Class " + typeClasse.getCanonicalName() + " missing default constructor", e);  
-		} catch (TransportException | JSONException |  IllegalAccessException | 
+			parseEntities(list);
+		} catch (TransportException | JSONException | 
 				MalformedURLException | URISyntaxException e) {
 			// Wrap checked exceptions in a RuntimeException.
 			// Checked exceptions are warped in a RuntimeException to reduce the need for
@@ -80,7 +58,66 @@ public class ModelIterator<T extends JSONParser> implements Iterable<T> {
 			throw new RuntimeException(e);  
 		} 
 	}
+	
+	/**
+	 * Finds and extracts array elements for iteration and offers them to q.
+	 * Override to handle special parsing requirements such as when base entity
+	 * contains multiple array elements at the root level.
+	 * 
+	 * @param rootObj The base, or root, element returned by the Mailchimp API.
+	 */
+	protected void parseEntities(final JSONObject rootObj) {
+		parseRoot(rootObj);
 
+		Iterator<String> keys = rootObj.keys();
+		while(keys.hasNext()) {
+			final String key = keys.next();
+			if (key.equals("_links")) { continue; }
+			final Object keyValue = rootObj.get(key);
+			if (keyValue instanceof JSONArray) { // look for main entity array
+				// TODO: TRACE -- found 'key' entity array of type T
+				final JSONArray entArray = (JSONArray)keyValue;
+				for (int i = 0 ; i < entArray.length();i++)
+				{
+					final JSONObject objDetail = entArray.getJSONObject(i);
+					q.offer(buildRefObj(connection, objDetail));
+				}
+				break;	// found entity array, no need to keep looking
+			}
+		}
+	}
+
+	/**
+	 * Extracts total_items as a hint to the total number of items to be iterated.
+	 * Override to allow superclass to extract additional root level entities.
+	 * 
+	 * @param arrayObj
+	 */
+	protected void parseRoot(JSONObject arrayObj) {
+		if (arrayObj.has("total_items")) {
+			totalItems = new Integer(arrayObj.getInt("total_items"));	// The total number of items matching the query regardless of pagination
+		}
+	}
+	
+	/**
+	 * Constructs type T using reflection. Override to provide a concrete constructor.
+	 * @param con
+	 * @param objDetail
+	 * @return
+	 */
+	protected T buildRefObj(MailChimpConnection con, JSONObject objDetail) {
+		try {
+			T ent;
+			ent = typeClasse.newInstance();
+			ent.parse(connection, objDetail);
+			return ent;
+		} catch (InstantiationException e) {
+			throw new RuntimeException("Class " + typeClasse.getCanonicalName() + " missing default constructor", e);  
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e); 
+		}
+	}
+	
 	@Override
 	public Iterator<T> iterator() {
 		Iterator<T> it = new Iterator<T>() {
